29d55e27207f1010d4e826d605c7ecb32b93c3d3
KenDerick <chenemiken15@gmail.com>
2020-04-22 21:51:34 +0100

    new commit

--- day2.py
+++ day2.py
@@ -1,3 +1,16 @@
 
-# Complete the sockMerchant function below.
 def sockMerchant(n, ar):
+    arset = set(ar)
+    counts = []
+    for i in arset:
+        v=0
+        for j in ar:
+            if j == i:
+                v+=1
+        counts.append(v)
+
+    pairs = 0
+    for i in counts:
+        y = i//2
+        pairs+=y
+    return pairs
